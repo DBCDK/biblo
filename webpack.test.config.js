6694efb1b0986884ab5adff33bddea89e93c3cdf
Jonas Peter Hyatt <jph@dbc.dk>
2016-02-16 14:15:35 +0100

    profile stuff

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -31,7 +31,8 @@ module.exports = {
         loader: 'babel',
         exclude: /node_modules/,
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015'],
+          plugins: ['transform-runtime', 'transform-async-to-generator']
         }
       },
       {

b73a2a514f72c0b814614dee1be826b5dd9d52d0
Jonas Peter Hyatt <jph@dbc.dk>
2016-03-01 12:33:38 +0100

    sd-429: tilføj pagelayout og sæt mus til pointer ved upload.

--- src/client/components/Profile/Edit/ProfileEditContainer.component.js
+++ src/client/components/Profile/Edit/ProfileEditContainer.component.js
@@ -5,6 +5,7 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 import ProfileForm from './profileForm.component';
+import PageLayout from '../../Layout/PageLayout.component';
 
 import * as ProfileActions from '../../../Actions/profile.actions';
 import * as EntitySuggestLibraryActions from '../../../Actions/entitySuggetLibrary.actions';
@@ -31,37 +32,39 @@ export default class ProfileEditContainer extends React.Component {
     });
 
     return (
-      <div>
-        <h1 className="profile-edit--title">Redigér Profil</h1>
-        <div className="profile-edit--form-component-container-container">
-          <div className="before-profile-edit--form-component-container">
-            <br />
-          </div>
-          <div className="profile-edit--form-component-container">
-            <ProfileForm
-              changeImageAction={this.props.actions.getProfile}
-              submit={this.imageSubmit}
-              errors={this.props.profile.errors || []}
-              profileImageSrc={((this.props.profile.image || {}).url).small}
-              submitState={this.props.profile.submitState}
-              submitProgress={this.props.profile.submitProgress}
-              fullName={this.props.profile.fullName}
-              birthday={this.props.profile.birthday}
-              phone={this.props.profile.phone}
-              displayName={this.props.profile.displayName}
-              description={this.props.profile.description}
-              email={this.props.profile.email}
-              favoriteLibrary={this.props.profile.favoriteLibrary || {}}
-              searchAction={this.librarySearch.bind(this)}
-              searchElements={searchElements}
-              unselectLibraryFunction={this.props.libraryActions.unselectLibrary}
-            />
-          </div>
-          <div className="after-profile-edit--form-component-container">
-            <br />
+      <PageLayout>
+        <div className="profile-edit--container">
+          <h1 className="profile-edit--title">Redigér Profil</h1>
+          <div className="profile-edit--form-component-container-container">
+            <div className="before-profile-edit--form-component-container">
+              <br />
+            </div>
+            <div className="profile-edit--form-component-container">
+              <ProfileForm
+                changeImageAction={this.props.actions.getProfile}
+                submit={this.imageSubmit}
+                errors={this.props.profile.errors || []}
+                profileImageSrc={((this.props.profile.image || {}).url).small}
+                submitState={this.props.profile.submitState}
+                submitProgress={this.props.profile.submitProgress}
+                fullName={this.props.profile.fullName}
+                birthday={this.props.profile.birthday}
+                phone={this.props.profile.phone}
+                displayName={this.props.profile.displayName}
+                description={this.props.profile.description}
+                email={this.props.profile.email}
+                favoriteLibrary={this.props.profile.favoriteLibrary || {}}
+                searchAction={this.librarySearch.bind(this)}
+                searchElements={searchElements}
+                unselectLibraryFunction={this.props.libraryActions.unselectLibrary}
+              />
+            </div>
+            <div className="after-profile-edit--form-component-container">
+              <br />
+            </div>
           </div>
         </div>
-      </div>
+      </PageLayout>
     );
   }
 }

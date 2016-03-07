4a5ce3ab836f2d90d7ee2494bc123d9fb2c0e215
Jacob Andresen <jan@dbc.dk>
2016-03-07 13:15:31 +0100

    basic group tile styling

--- src/client/components/Groups/View/GroupViewTile.component.js
+++ src/client/components/Groups/View/GroupViewTile.component.js
@@ -5,33 +5,61 @@ import './scss/group-view.scss';
 
 export default class GroupViewTile extends React.Component {
 
-  render() {
-    var group = this.props.group;
-    console.log(group);
-    if (group.coverImage) {
-        group.url = "//api"
-    }
-    group.url = "http://lorempixel.com/120/120/";
+  getMembersCountString() {
+    var groupStr,
+       group = this.props.group;
 
-    var groupStr;
-    if (group.membersCount == 1 ) {
-        groupStr = "1 følger"
-    }  else {
-        groupStr = group.membersCount + " følgere";
+    if (group.membersCount == 1) {
+      groupStr = "1 følger"
+    } else {
+      groupStr = group.membersCount + " følgere";
     }
+    return groupStr;
+  }
+
+  render() {
+    var group    = this.props.group,
+        groupStr = this.getMembersCountString();
+
+    if (group.membersCount == 1) {
+      groupStr = "1 følger"
+    } else {
+      groupStr = group.membersCount + " følgere";
+    }
+
     var groupUrl = "/grupper/" + group.id;
 
     return (
-       <div className="group--tile" key={group.id}>
-          <a href={groupUrl}><img src={group.url} /></a>
-          <div className="group--title">{group.name} </div>
-          <div className="group--followers">{groupStr} </div>
-       </div>
-   )}
+      <div className="group--tile" key={group.id}>
+        <div>
+        <a href={groupUrl}>
+          <img className="group--coverimage" src={this.getCoverImageUrl()} align="middle" />
+          <img className="group--coverbadge" src="no_group_image.png" />
+        </a>
+        </div>
+        <div className="group--caption">
+           <div className="group--title">{group.name} </div>
+           <div className="group--followers">{groupStr} </div>
+        </div>
+      </div>
+    )
+  }
+
+  getCoverImageUrl() {
+    var url,
+      group = this.props.group;
+    if (group.coverImage) {
+      url = "/billede/" + group.coverImage.id +"/small"
+    } else {
+      url = "http://lorempixel.com/120/120/";
+    }
+    return url;
+  }
+
 }
 
 GroupViewTile.propTypes = {
-    group: React.PropTypes.object.isRequired
+  group: React.PropTypes.object.isRequired
 };
 
 GroupViewTile.displayName = 'GroupViewTile';

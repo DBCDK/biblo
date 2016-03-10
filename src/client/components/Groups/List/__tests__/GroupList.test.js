dbd71dd69f59eb63b369f2a7f0a1aabc0f9b3bda
Jacob Andresen <jan@dbc.dk>
2016-03-10 13:50:48 +0100

    lint tests

--- src/client/components/Groups/List/__tests__/GroupList.test.js
+++ src/client/components/Groups/List/__tests__/GroupList.test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-import {expect} from 'chai';
+// import React from 'react';
+// import TestUtils from 'react-addons-test-utils';
+// import {expect} from 'chai';
 
-import GroupList from '../GroupList.component';
+// import GroupList from '../GroupList.component';
 
 describe('Test of GroupList', () => {
 
-    //NOTE: this is tested via ExpandButton
-    //it('Should expand the number of groups show when clicking on the expand button', () => {
-    //});
+    // NOTE: this is tested via ExpandButton
+    // it('Should expand the number of groups show when clicking on the expand button', () => {
+    // });
 
 });
--- src/client/components/Groups/__tests__/GroupsContainer.component.test.js
+++ src/client/components/Groups/__tests__/GroupsContainer.component.test.js
@@ -11,25 +11,24 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import {GroupsContainer} from '../GroupsContainer.component';
 
-
 describe('GroupsContainer component tests', () => {
   it('should test groups container can render', () => {
 
     var data = {
-        groups: [
-          {id: 1, name: 'heste test', membersCount: 1}
-        ],
-        groupsLimit: 15
-      };
-
+      groups: [
+        {id: 1, name: 'heste test', membersCount: 1}
+      ],
+      groupsLimit: 15
+    };
 
     var actions = {
       asyncShowGroups: function () {
       }
-    }
+    };
 
     const comp = TestUtils.renderIntoDocument(<GroupsContainer data={data} actions={actions}/>);
-    expect(ReactDOM.findDOMNode(comp).children[1].children[1].textContent).toEqual('Opret ny gruppeNyeste grupper heste test '); //we currenly do not expect followers count on screen
+    expect(ReactDOM.findDOMNode(comp).children[1].children[1].textContent)
+      .toEqual('Opret ny gruppeNyeste grupper heste test '); // we currently do not expect followers count on screen
 
   });
 });

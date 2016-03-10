ddaf745406bc3821fb6db228ac9e8858082790f5
Jacob Andresen <jan@dbc.dk>
2016-03-10 14:41:55 +0100

    add basic grouplist test

--- src/client/components/Groups/List/__tests__/GroupList.test.js
+++ src/client/components/Groups/List/__tests__/GroupList.test.js
@@ -1,15 +1,27 @@
 'use strict';
 
-// import React from 'react';
-// import TestUtils from 'react-addons-test-utils';
-// import {expect} from 'chai';
 
-// import GroupList from '../GroupList.component';
+import expect from 'expect';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+import GroupList from '../GroupList.component';
 
 describe('Test of GroupList', () => {
 
-    // NOTE: this is tested via ExpandButton
-    // it('Should expand the number of groups show when clicking on the expand button', () => {
-    // });
+  it('Should render', () => {
+
+    var data = {
+      groups: [
+        {id: 1, name: 'heste test', membersCount: 1}
+      ],
+      groupsLimit: 15
+    };
+
+    const comp = TestUtils.renderIntoDocument(<GroupList groups={data.groups}/>);
+  })
+
 
 });

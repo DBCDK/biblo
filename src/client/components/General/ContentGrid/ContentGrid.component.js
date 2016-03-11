536c0a5a69f077789252af508cd5cf203ac99296
Matias Rasmussen <rasmussen.matias@gmail.com>
2016-03-11 15:18:44 +0100

    Refactored colored  header + Frontpage

---
+++ src/client/components/General/ContentGrid/ContentGrid.component.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import React from 'react';
+
+import ContentGridElement from './ContentGridElement.component.js';
+
+import './content-grid.scss';
+
+function ContentGrid({items=[]}) {
+  
+  const elements = items.map((e) => {
+    return (
+      <ContentGridElement
+        key={e.id}
+        title={e.title}
+        text={e.text}
+        url={e.url}
+        imageUrl={e.imageUrl}
+        />
+    );
+  });
+
+
+  return (
+    <div className="content-grid">
+      <ul>
+        {elements}
+      </ul>
+    </div>
+  );
+}
+
+ContentGrid.displayName = 'ContentGrid';
+
+ContentGrid.propTypes = {
+  items: React.PropTypes.array
+};
+
+export default ContentGrid;
--- src/client/components/Groups/GroupsContainer.component.js
+++ src/client/components/Groups/GroupsContainer.component.js
@@ -11,38 +11,29 @@ import PageLayout from '../Layout/PageLayout.component.js';
 
 import GroupList from './List/GroupList.component';
 import RoundedButton from '../General/RoundedButton/RoundedButton.a.component.js';
+import ColoredHeader from '../General/ColoredHeader/ColoredHeader.component.js';
 import {CREATE_GROUP_LINK} from '../../Constants/hyperlinks.constants';
 
-import Icon from '../General/Icon/Icon.component.js';
 import groupSvg from '../General/Icon/svg/functions/group.svg';
 
 import './GroupsContainer.scss';
 
 
 export class GroupsContainer extends React.Component {
+
+
+
   render() {
+    const text = 'Her kan du deltage, se hvad andre skriver og uploader. Du kan også lave din egen gruppe - om det du interesserer dig mest for. Du kan spørge gruppeværterne i alle grupperne om mere information.';
     const {data, actions} = this.props;
     return (
       <PageLayout>
-        <div className="header">
-          <div className="iconcontainer">
-            <Icon className="icon" height='100' width='100' glyph={groupSvg}/>
-          </div>
-          <div className="description">
-            <div className="text">
-            <h2>Grupper</h2>
-            Her kan du deltage, se hvad andre skriver og uploader. Du kan også lave din egen gruppe - om det du
-            interesserer
-            dig mest for. Du kan spørge gruppeværterne i alle grupperne om mere information.
-              </div>
-          </div>
-        </div>
+        <ColoredHeader text={text} title={'Grupper'} iconGlyph={groupSvg} />
         <div className="lists">
           <RoundedButton buttonText='Opret ny gruppe' href={CREATE_GROUP_LINK}/>
           <GroupList title="Nyeste grupper" groups={data.groups} limit={data.groupsLimit}
                      isLoading={data.loadingGroups} expand={actions.asyncShowGroups} actions={actions}/>
         </div>
-
       </PageLayout>
     );
   }

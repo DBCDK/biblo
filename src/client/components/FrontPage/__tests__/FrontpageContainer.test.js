1891f330408993e93766f8a3fd4c7325ca7cba6e
Matias Rasmussen <rasmussen.matias@gmail.com>
2016-03-14 09:59:29 +0100

    Lint fix

--- src/client/components/FrontPage/__tests__/FrontpageContainer.test.js
+++ src/client/components/FrontPage/__tests__/FrontpageContainer.test.js
@@ -4,13 +4,5 @@
  * @file
  */
 
-import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-import {assert} from 'chai';
-
-import FrontpageContainer from '../FrontpageContainer.component';
-
 describe('Test frontpage', () => {
-
-  
 });

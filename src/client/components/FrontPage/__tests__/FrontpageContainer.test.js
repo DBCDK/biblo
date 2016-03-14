d84df0f86e3abe64043d3e2f399f79fa08fcaaa2
Matias Rasmussen <rasmussen.matias@gmail.com>
2016-03-14 09:50:37 +0100

    Removed test

--- src/client/components/FrontPage/__tests__/FrontpageContainer.test.js
+++ src/client/components/FrontPage/__tests__/FrontpageContainer.test.js
@@ -12,14 +12,5 @@ import FrontpageContainer from '../FrontpageContainer.component';
 
 describe('Test frontpage', () => {
 
-  it('Assert Biblo is present on frontpage', () => {
-    const render = TestUtils.createRenderer();
-    render.render(<FrontpageContainer />);
-
-    const rendered = render.getRenderOutput();
-
-    const result = rendered.props.children[0].props.children;
-    const expected = 'Biblo';
-    assert.equal(result, expected, 'Found Biblo as expected');
-  });
+  
 });

0ea4148f79dd783d3936475e673ee6e11b9fd4c3
Jacob Andresen <jan@dbc.dk>
2016-04-13 16:41:10 +0200

    using dummy work data

--- src/client/components/Work/WorkContainer.component.js
+++ src/client/components/Work/WorkContainer.component.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 
-import PageLayout from '../Layout/PageLayout.component.js';
+import * as reviewActions from '../../Actions/review.actions';
+import PageLayout from '../Layout/PageLayout.component';
+import ReviewView from '../Review/ReviewView.component';
 
 export class WorkContainer extends React.Component {
   render() {
-
     const jsonData = document.getElementById('JSONDATA');
     const data = JSON.parse(jsonData.innerHTML);
     const work = data.work;
 
+    let profile = {
+      id: 1
+    };
+
     return (
       <PageLayout>
         <p>WorkContainer {work.id} {work.title}</p>
+        <ReviewView
+          profile={profile}
+          pid={work.id}
+          worktype="book"
+          reviewActions={reviewActions}
+        />
       </PageLayout>
     );
   }
@@ -19,5 +32,20 @@ export class WorkContainer extends React.Component {
 
 WorkContainer.displayName = 'WorkContainer';
 WorkContainer.propTypes = {
-
+  actions: React.PropTypes.object.isRequired,
+  data: React.PropTypes.object
 };
+
+export default connect(
+  (state) => {
+    return {
+      data: state.reviewReducer
+    };
+  },
+
+  (dispatch) => {
+    return {
+      actions: bindActionCreators(reviewActions, dispatch)
+    };
+  }
+)(WorkContainer);

0db5f9d964f09aea6d84e4a5a803173410000c1b
Jacob Andresen <jan@dbc.dk>
2016-04-15 16:39:49 +0200

    upsert approach

--- src/server/serviceProvider/transformers/Community/Reviews/createReview.transform.js
+++ src/server/serviceProvider/transformers/Community/Reviews/createReview.transform.js
@@ -3,19 +3,46 @@ const CreateReviewTransform = {
     return 'createReview';
   },
 
-  requestTransform(event, query, connection) {
-    const passport = connection.request.session.passport;
-    if (passport) {
-      return this.callServiceClient('community', 'createReview', {
-        pid: query.pid,
-        content: query.content,
-        worktype: query.worktype,
-        rating: query.rating,
-        created: Date.now(),
-        modified: Date.now(),
-        reviewownerid: passport.profileId
-      });
+  upsertContent(query, user) {
+    let method="createReview";
+    if (review.id) {
+      method= "updateReview";
     }
+
+    return this.callServiceClient('community', method, {
+      id: query.id || null,
+      pid: query.pid,
+      content: query.content,
+      worktype: query.worktype,
+      rating: query.rating,
+      created: querycreated || Date.now(),
+      modified: Date.now(),
+      reviewownerid: user.profileId
+    }).then((response) => {
+
+      //attach image to review
+      if (response.statusCode === 200 && review.image) {
+        const image = query.image;
+        query.image = {data: 'Binary Image Data!'};
+        return this.callServiceClient('community', 'updateImage', {
+          relationId: response.body.id,
+          image,
+          accessToken: user.id,
+          relationType: 'reviewImageCollection'
+        }).then(() => {
+          return response;
+        });
+
+        //vide is attach on community server
+      }
+      return response;
+    });
+  },
+
+  requestTransform(event, query, connection) {
+    const user = Object.assign({}, connection.request.session.passport.user);
+
+    this.upserConntent(query,  user);
     return Promise.reject(new Error('user not logged in'));
   },
 

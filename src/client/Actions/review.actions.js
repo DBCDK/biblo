2172719ec00d3c58caa9ce46fa02d7a61f111df1
Jacob Andresen <jan@dbc.dk>
2016-04-15 16:35:54 +0200

    use upsert approach

--- src/client/Actions/review.actions.js
+++ src/client/Actions/review.actions.js
@@ -4,7 +4,6 @@ import * as types from '../Constants/action.constants';
 import SocketClient from 'dbc-node-serviceprovider-socketclient';
 
 const createReviewClient = SocketClient('createReview');
-const updateReviewClient = SocketClient('updateReview');
 
 export function asyncCreateReview(review) {
   return (dispatch) => {
@@ -19,23 +18,6 @@ export function asyncCreateReview(review) {
 export function createReview(review) {
   return {
     type: types.CREATE_REVIEW,
-    review: review
-  };
-}
-
-export function asyncUpdateReview(review) {
-  return (dispatch) => {
-    updateReviewClient.request(review);
-    const event = updateReviewClient.response(function () {
-      dispatch(updateReview(review));
-      event.off();
-    });
-  };
-}
-
-export function updateReview(review) {
-  return {
-    type: types.UPDATE_REVIEW,
     review: review
   };
 }

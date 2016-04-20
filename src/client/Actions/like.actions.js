06189a38812c5c7c3bdd1955fe7b9c8fc9a46cd7
Jacob Andresen <jan@dbc.dk>
2016-04-20 15:19:00 +0200

    like review

--- src/client/Actions/like.actions.js
+++ src/client/Actions/like.actions.js
@@ -9,6 +9,10 @@ import SocketClient from 'dbc-node-serviceprovider-socketclient';
 const likePostSocketClient = SocketClient('likePost');
 const unlikePostSocketClient = SocketClient('unlikePost');
 
+const likeReviewSocketClient = SocketClient('likeReviewt');
+const unlikeReviewSocketClient = SocketClient('unlikeReview');
+
+
 export function likePost(like) {
   likePostSocketClient.request({
     profileId: like.profileId,
@@ -32,3 +36,29 @@ export function unlikePost(like) {
     postId: like.postId
   };
 }
+
+
+export function likeReview(like) {
+  likeReviewSocketClient.request({
+    profileId: like.profileId,
+    reviewId: like.reviewId
+  });
+  return {
+    type: types.LIKE_REVIEW,
+    profileId: like.profileId,
+    reviewId: like.reviewId
+  };
+}
+
+export function unlikeReview(like) {
+  unlikeReviewSocketClient.request({
+    profileId: like.profileId,
+    reviewId: like.reviewId
+  });
+  return {
+    type: types.UNLIKE_REVIEW,
+    profileId: like.profileId,
+    reviewId: like.reviewId
+  };
+}
+

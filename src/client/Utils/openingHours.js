606b41a4673fe5981dd16070ab95a54d475716ea
Matias Rasmussen <rasmussen.matias@gmail.com>
2016-04-25 16:12:43 +0200

    Biblo closes at 21:00

--- src/client/Utils/openingHours.js
+++ src/client/Utils/openingHours.js
@@ -7,7 +7,7 @@
 export default function isSiteOpen() {
   // check if site is open (09:00 - 21:00)
   const opensAtHour = 9;
-  const closesAtHour = 14;
+  const closesAtHour = 21;
   const hour = (new Date()).getHours();
   const isSiteOpen = hour >= opensAtHour && hour < closesAtHour;
   return isSiteOpen;

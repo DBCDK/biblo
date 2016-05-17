102a12c176260ed07f7c203650a0ced554cd4979
Jonas Peter Hyatt <jph@dbc.dk>
2016-05-17 07:13:54 +0200

    sd-511: styling af skriftelig anmeldelse

--- src/client/components/WidgetContainer/widgets/LatestReviews/LatestReviews.widget.component.js
+++ src/client/components/WidgetContainer/widgets/LatestReviews/LatestReviews.widget.component.js
@@ -3,14 +3,20 @@
  */
 
 import {Component, PropTypes} from 'react';
+
 import {CompactReviewElement} from './CompactReviewElement.component';
+import Icon from '../../../General/Icon/Icon.component';
+
+import plusSvg from '../../../General/Icon/svg/functions/plus.svg';
 
 import './LatestReviews.widget.component.scss';
 
 export class LatestReviewsWidget extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isClosed: true
+    };
   }
 
   componentDidMount() {
@@ -21,21 +27,35 @@ export class LatestReviewsWidget extends Component {
   render() {
     let {displayTitle} = this.props.widgetData;
     let reviews = this.props.widgetReducerProp;
+    let classNames = 'latest-reviews-widget--reviews-container';
+
+    if (this.state.isClosed) {
+      classNames += ' closed';
+    }
 
     reviews = reviews.map((review) => {
       return (
-        <CompactReviewElement 
-          key={`compact_review_${review.id}`} 
+        <CompactReviewElement
+          key={`compact_review_${review.id}`}
           review={review}
           coverImages={this.props.widgetState.CoverImages || {}}
-          getCoverImageAction={this.props.widgetActions.asyncGetCoverImage} />
+          getCoverImageAction={this.props.widgetActions.asyncGetCoverImage}/>
       );
     });
 
     return (
       <div>
-        <h2>{displayTitle}</h2>
-        <div className="latest-reviews-widget--reviews-container">
+        <div className="latest-reviews-widget--widget-title--and--show-more-button--container">
+          <h2>{displayTitle}</h2>
+          <a
+            className="latest-reviews-widget--show-more-button"
+            onClick={() => this.setState({isClosed: !this.state.isClosed})}
+            >
+            <Icon glyph={plusSvg} />
+            {this.state.isClosed ? ' VIS FLERE' : ' VIS FÆRRE'}
+          </a>
+        </div>
+        <div className={classNames}>
           {reviews}
         </div>
       </div>

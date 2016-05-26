1ba89f023c2770bc0b1ab65dcf5f65e5e17f0191
Matias Rasmussen <rasmussen.matias@gmail.com>
2016-05-26 16:07:48 +0200

    use url instead of relative file path for images in pdf generator

--- src/app.js
+++ src/app.js
@@ -260,7 +260,7 @@ module.exports.run = function(worker) {
   // Graceful handling of errors
   app.use((err, req, res, next) => {
     logger.log('error', 'An error occurred! Got following: ' + err);
-    console.error('error', 'An error occurred! Got following: ' + err); // eslint-disable-line no-console
+    console.error('error', 'An error occurred! Got following: ' + err.stack); // eslint-disable-line no-console
     if (res.headersSent) {
       return next(err);
     }

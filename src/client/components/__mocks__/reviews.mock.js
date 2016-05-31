9e2af06ec45893105e00fa6d51d1ab21d4647c1a
Martin Møller <mmj@dbc.dk>
2016-05-31 12:46:40 +0200

    FIxer issue/46: Læsebevis til Sommerbogen på profil skal ikke vises på offentlig profil

---
+++ src/client/components/__mocks__/reviews.mock.js
@@ -0,0 +1,138 @@
+export const reviewsMock = [
+  {
+    pid: '870970-basis:22252852',
+    libraryid: '714700',
+    worktype: 'book',
+    content: 'Helt max opperen bog dér!',
+    created: '2016-05-25T13:24:03.000Z',
+    modified: '2016-05-25T13:24:03.000Z',
+    rating: 6,
+    markedAsDeleted: null,
+    palleid: null,
+    id: 291622,
+    reviewownerid: 9,
+    likes: [],
+    video: {
+      id: 12,
+      postVideoCollection: null,
+      commentVideoCollection: null,
+      reviewVideoCollection: 291622,
+      resolutions: [
+        {
+          size: 'original_video',
+          id: 895,
+          videoCollectionResolutionId: 12,
+          imageCollectionResolutionId: null,
+          video: {
+            container: 'uxdev-biblo-input-videobucket',
+            name: '1464182623321_profile_9.mov',
+            type: 'video/quicktime',
+            url: 'api/fileContainers/uxdev-biblo-input-videobucket/download/1464182623321_profile_9.mov',
+            id: 897,
+            resolutionImageFileId: null,
+            resolutionVideoFileId: 895
+          }
+        }]
+    },
+    campaign: {
+      id: 1,
+      campaignName: 'Sommerbogen 2016',
+      workTypes: ['book', 'literature'],
+      startDate: '2016-01-16T15:06:53.044Z',
+      endDate: '2016-06-16T15:06:53.044Z',
+      logos: {
+        svg: '/sommerbogen-logo.svg',
+        small: '/sommerbogen-logo.png',
+        medium: '/sommerbogen-logo.png',
+        large: '/sommerbogen-logo.png'
+      }
+    },
+    owner: {
+      raw: {},
+      description: '',
+      displayName: 'Anonym',
+      id: 0,
+      groups: [],
+      image: '/no_profile.png',
+      isModerator: false
+    },
+    image: null,
+    html: 'Helt max opperen bog dér!'
+  },
+  {
+    pid: '870970-basis:51115155',
+    libraryid: '714700',
+    worktype: 'book',
+    content: 'Super super fed bog! Den er bare verdens bedste og den kan aldrig blive dårlig!',
+    created: '2016-05-18T12:03:06.000Z',
+    modified: '2016-05-18T12:03:06.000Z',
+    rating: 6,
+    markedAsDeleted: null,
+    palleid: null,
+    id: 291551,
+    reviewownerid: 9,
+    likes: [9],
+    campaign: {
+      id: 1,
+      campaignName: 'Sommerbogen 2016',
+      workTypes: ['book', 'literature'],
+      startDate: '2016-01-16T15:06:53.044Z',
+      endDate: '2016-06-16T15:06:53.044Z',
+      logos: {
+        svg: '/sommerbogen-logo.svg',
+        small: '/sommerbogen-logo.png',
+        medium: '/sommerbogen-logo.png',
+        large: '/sommerbogen-logo.png'
+      }
+    },
+    owner: {
+      raw: {},
+      description: '',
+      displayName: 'Anonym',
+      id: 0,
+      groups: [],
+      image: '/no_profile.png',
+      isModerator: false
+    },
+    image: null,
+    html: 'Super super fed bog! Den er bare verdens bedste og den kan aldrig blive dårlig!'
+  },
+  {
+    pid: '870970-basis:22467859',
+    libraryid: '714703',
+    worktype: 'book',
+    content: 'hest',
+    created: '2016-05-04T10:32:29.348Z',
+    modified: '2016-05-04T10:32:29.348Z',
+    rating: 1,
+    markedAsDeleted: null,
+    palleid: null,
+    id: 22,
+    reviewownerid: 9,
+    likes: [],
+    campaign: {
+      id: 1,
+      campaignName: 'Sommerbogen 2016',
+      workTypes: ['book', 'literature'],
+      startDate: '2016-01-16T15:06:53.044Z',
+      endDate: '2016-06-16T15:06:53.044Z',
+      logos: {
+        svg: '/sommerbogen-logo.svg',
+        small: '/sommerbogen-logo.png',
+        medium: '/sommerbogen-logo.png',
+        large: '/sommerbogen-logo.png'
+      }
+    },
+    owner: {
+      raw: {},
+      description: '',
+      displayName: 'Anonym',
+      id: 0,
+      groups: [],
+      image: '/no_profile.png',
+      isModerator: false
+    },
+    image: null,
+    html: 'hest'
+  }
+];

58f5ae299eed3779c47b443c173e953c7474385a
Jonas Peter Hyatt <jph@dbc.dk>
2016-05-31 10:37:02 +0200

    bug-33: Rewrite transform to better use relations in loopback :)

--- src/server/serviceProvider/transformers/Community/Groups/getGroup.transform.js
+++ src/server/serviceProvider/transformers/Community/Groups/getGroup.transform.js
@@ -1,3 +1,7 @@
+/**
+ * @file: This transform gets a group from the community service, if a user is logged in, it also checks if that user is following the group.
+ */
+
 import * as _ from 'lodash';
 import parseProfile from '../../../parsers/profile.parser';
 import groupParser from '../../../parsers/group.parser';
@@ -48,7 +52,7 @@ const GetGroupTransform = {
 
   responseTransform(response, query, connection) {
     let body = groupParser(JSON.parse(response[0].body));
-    body.isFollowing = response[1] && response[1].statusCode && response[1].statusCode !== 404 || false;
+    body.isFollowing = response[1] && response[1].statusCode && response[1].statusCode !== 404 || false; // If the status code is 404, the user is not following the group
     body.members = (_.filter(body.members, (member) => member.id !== body.owner.id)).map((member) => {
       return parseProfile(member, true, 'small');
     });

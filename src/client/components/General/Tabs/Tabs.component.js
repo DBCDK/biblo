24240cbcb4a40be2f610f28e968466ec5cd84907
Martin Møller <mmj@dbc.dk>
2016-06-17 15:16:30 +0200

    Added loging when tabs are clicked

--- src/client/components/General/Tabs/Tabs.component.js
+++ src/client/components/General/Tabs/Tabs.component.js
@@ -3,6 +3,7 @@
  */
 
 import React from 'react';
+import * as newrelicWrapper from '../../../Utils/newrelicWrapper';
 
 import Icon from '../../General/Icon/Icon.component';
 
@@ -19,6 +20,10 @@ export default class Tabs extends React.Component {
 
   onClicked(index) {
     this.setState({selected: index});
+    newrelicWrapper.addPageAction('Tab was clicked', {
+      from_tab: this.props.tabs[this.state.selected],
+      to_tab: this.props.tabs[index]
+    });
   }
 
   componentWillMount() {
@@ -46,7 +51,7 @@ export default class Tabs extends React.Component {
       const icon = pane.icon ? this.getIcon(pane.icon) : null;
       return (
         <li className={activeClass} key={index} >
-          <a href={`#${pane.label}`} onClick={this.onClicked.bind(this, index)} >{icon}<span className="tab--label">{pane.label}</span></a>
+          <a href={`#${pane.label}`} onClick={this.onClicked.bind(this, index)} >{icon}<span className="tab--label" >{pane.label}</span></a>
         </li>
       );
     });

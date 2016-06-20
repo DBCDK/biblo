b0aa8016d30b34c5d2d9d4e97a6c7a9fda07d085
Jonas Peter Hyatt <jph@dbc.dk>
2016-06-20 13:06:36 +0200

    bug-123: Add a test to check login logout functionality

--- selenium/features/support/world.js
+++ selenium/features/support/world.js
@@ -8,14 +8,14 @@ function World() {
   this.driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome());
   this.browser = this.driver.build();
 
-  this.click = selector => {
-    this.browser.wait(until.elementIsVisible(this.browser.findElement(By.css(selector))), driverTimeout);
-    return this.browser.findElement(By.css(selector)).click();
-  }
-
   this.$ = selector => {
     return this.browser.findElement(By.css(selector));
-  }
+  };
+
+  this.click = selector => {
+    this.browser.wait(until.elementIsVisible(this.$(selector)), driverTimeout);
+    return this.$(selector).click();
+  };
 }
 
 module.exports = function() {

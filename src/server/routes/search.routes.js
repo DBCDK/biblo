e3501758277c3b93967b45163ad24c65b9bf84cb
Jacob Andresen <jan@dbc.dk>
2016-06-23 09:48:33 +0200

    decodeURIComponent on searchGroups and trycatch

--- src/server/routes/search.routes.js
+++ src/server/routes/search.routes.js
@@ -8,40 +8,46 @@ const SearchRoutes = express.Router();
 
 SearchRoutes.get('/', async function (req, res, next) {
 
-  // get search query from url
-  const params = {
-    q: (req.query.q) ? decodeURIComponent(req.query.q) : null,
-    forfatter: (req.query.forfatter) ? decodeURIComponent(req.query.forfatter) : null,
-    materialer: (req.query.materialer) ? decodeURIComponent(req.query.materialer) : null,
-    emneord: (req.query.emneord) ? decodeURIComponent(req.query.emneord) : null,
-    limit: 5
-  };
+  try {
+    // get search query from url
+    const params = {
+      q: (req.query.q) ? decodeURIComponent(req.query.q) : null,
+      forfatter: (req.query.forfatter) ? decodeURIComponent(req.query.forfatter) : null,
+      materialer: (req.query.materialer) ? decodeURIComponent(req.query.materialer) : null,
+      emneord: (req.query.emneord) ? decodeURIComponent(req.query.emneord) : null,
+      limit: 5
+    };
 
-  // call Community service search (we expect this to be fast so we wait for the result here)
-  let groupSearchResults = await req.callServiceProvider('searchGroups', {
-    q: req.query.q, limit: 5
-  });
-
-  groupSearchResults = groupSearchResults[0];
-
-  // call Open-Platform search endpoint
-  req.callServiceProvider('search', params)
-    .then((stuff) => {
-      const materialSearchResults = stuff[0].data;
-      res.locals.title = `${params.q} - Søgning - Biblo.dk`;
-      res.render('page', {
-        css: ['/css/search.css'],
-        js: ['/js/search.js'],
-        jsonData: [JSON.stringify({
-          materialSearchResults: materialSearchResults,
-          groupSearchResults: groupSearchResults,
-          query: params.q
-        })]
-      });
-    })
-    .catch((e) => {
-      next(e);
+    // call Community service search (we expect this to be fast so we wait for the result here)
+    let groupSearchResults = await req.callServiceProvider('searchGroups', {
+      q: (req.query.q) ? decodeURIComponent(req.query.q) : null,
+      limit: 5
     });
+
+    groupSearchResults = groupSearchResults[0];
+
+    // call Open-Platform search endpoint
+    req.callServiceProvider('search', params)
+      .then((stuff) => {
+        const materialSearchResults = stuff[0].data;
+        res.locals.title = `${params.q} - Søgning - Biblo.dk`;
+        res.render('page', {
+          css: ['/css/search.css'],
+          js: ['/js/search.js'],
+          jsonData: [JSON.stringify({
+            materialSearchResults: materialSearchResults,
+            groupSearchResults: groupSearchResults,
+            query: params.q
+          })]
+        });
+      })
+      .catch((e) => {
+        next(e);
+      });
+  }
+  catch (err) {
+    next(err);
+  }
 });
 
 export default SearchRoutes;

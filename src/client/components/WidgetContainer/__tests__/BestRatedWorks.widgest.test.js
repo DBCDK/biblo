f834972ce70b024779f32654ceabec6106f1c880
Jonas Peter Hyatt <jph@dbc.dk>
2016-07-18 12:30:33 +0200

    sd-522: Write new test for best rated works widget

---
+++ src/client/components/WidgetContainer/__tests__/BestRatedWorks.widgest.test.js
@@ -0,0 +1,58 @@
+/**
+ * @file: This file contains tests for the Best rated works widget.
+ **/
+
+// import libs
+import expect from 'expect';
+import {renderWidgetWithTeaspoon} from './widgetTest.utils';
+
+describe('Test BestRatedWorksWidget Widget', () => {
+  it('Test BestRatedWorksWidget can render inside a WidgetContainer', () => {
+    const $root = renderWidgetWithTeaspoon({
+      location: 'test-best-works-widget-location',
+      widgetName: 'BestRatedWorksWidget',
+      widgetConfig: {
+        title: 'BestRatedWorksWidget title Test!',
+        reviewsToLoad: 15,
+        showTitle: true
+      },
+      state: {
+        BestRatedWorks: {}
+      }
+    });
+
+    const LatestReviewsWidget = $root.find('h2').text();
+    expect(LatestReviewsWidget).toEqual('BestRatedWorksWidget title Test!');
+  });
+
+  it('should render a compactWorkElement when given correct props', () => {
+    const workTitle = 'Fisker og båd på Roskilde Fjord';
+    const $root = renderWidgetWithTeaspoon({
+      location: 'test-best-works-widget-location',
+      widgetName: 'BestRatedWorksWidget',
+      widgetConfig: {
+        title: 'BestRatedWorksWidget title Test!',
+        reviewsToLoad: 15,
+        showTitle: true
+      },
+      state: {
+        BestRatedWorks: {
+          isLoading: false,
+          works: [{
+            dcTitle: [workTitle],
+            collection: ['870970-basis:06903177'],
+            workType: ['book']
+          }]
+        }
+      }
+    });
+
+    let innerText = '';
+    $root
+      .find('.compact-work-element > .compact-work-element--title-and-worktype--container > .compact-work-element--title > :text')
+      .each(node => (innerText = innerText.concat(node._stringText)));
+
+    expect(innerText).toEqual(` ${workTitle}`);
+  });
+});
+
---
+++ src/client/components/WidgetContainer/__tests__/widgetTest.utils.js
@@ -0,0 +1,34 @@
+// import libs
+import React from 'react';
+import $ from 'teaspoon';
+
+// import components
+import WidgetContainer from '../WidgetContainer.component';
+
+export function renderWidgetWithTeaspoon(params) {
+  const widgetLocationName = params.location || 'test-widget-location';
+  const widgetState = Object.assign({
+    widgetLocations: {}
+  }, params.state);
+
+  const widgetActions = Object.assign({
+    asyncGetLatestReviews: () => {},
+    asyncListenForCoverImages: () => {},
+    asyncGetCoverImage: () => {},
+    callServiceProvider: () => {}
+  }, params.actions);
+
+  widgetState.widgetLocations[widgetLocationName] = {
+    widgetName: params.widgetName,
+    widgetConfig: params.widgetConfig
+  };
+
+  let component = (
+    <WidgetContainer
+      widgetLocationName={widgetLocationName}
+      widgetState={widgetState}
+      widgetActions={widgetActions} />
+  );
+
+  return $(component).render();
+}

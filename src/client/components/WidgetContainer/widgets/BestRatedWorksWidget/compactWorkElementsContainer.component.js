f26d28bf668f74c81f663bf2e5650afaf0c9e92e
Jonas Peter Hyatt <jph@dbc.dk>
2016-07-18 13:21:01 +0200

    sd-522: add centered spinner to aid waiting...

--- src/client/components/WidgetContainer/widgets/BestRatedWorksWidget/compactWorkElementsContainer.component.js
+++ src/client/components/WidgetContainer/widgets/BestRatedWorksWidget/compactWorkElementsContainer.component.js
@@ -2,8 +2,17 @@
  * @file: Compact work elements container, contains a gallery of compact works.
  */
 
+// Libraries
 import React, {Component, PropTypes} from 'react';
+
+// Components
 import {CompactWorkElement} from './compactWorkElement.component';
+import Icon from '../../../General/Icon/Icon.component';
+
+// SVG
+import spinnerSvg from '../../../General/Icon/svg/spinners/loading-spin.svg';
+
+// Styles
 import './scss/compactWorkElementsContainer.component.scss';
 
 export class CompactWorkElementsContainer extends Component {
@@ -12,12 +21,21 @@ export class CompactWorkElementsContainer extends Component {
   }
 
   render() {
-    const elements = this.props.works.map(work => <CompactWorkElement work={work} key={`work-${work.collection[0]}`} />);
+    const elements = this.props.works.map(work => <CompactWorkElement work={work} key={`work-${work.collection[0]}`}/>);
     const classNames = 'compact-works--container' + (this.props.closed ? ' closed' : '');
+
+    let spinner = <span />;
+
+    if (this.props.isLoading) {
+      spinner = (
+        <div className="compact-works--spinner-container">
+          <Icon glyph={spinnerSvg} height={150} width={150}/>
+        </div>);
+    }
 
     return (
       <div className={classNames}>
-        {this.props.isLoading ? <p>Indlæser!</p> : <span />}
+        {spinner}
         {elements}
       </div>
     );

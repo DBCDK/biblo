67d78d70940f7a774f4551169e1cd9fa45aa9d56
Jonas Peter Hyatt <jph@dbc.dk>
2016-07-18 15:20:29 +0200

    sd-522: Created a preview page for tests, created a cucumber test to check DummyWidget can be rendered.

---
+++ src/client/components/WidgetContainer/PreviewPage.component.js
@@ -0,0 +1,52 @@
+/* eslint-disable react/no-danger */
+
+import React from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+import PageLayout from '../Layout/PageLayout.component.js';
+import WidgetContainer from '../WidgetContainer/WidgetContainer.component';
+
+import * as searchActions from '../../Actions/search.actions';
+import * as widgetActions from '../../Actions/widget.actions';
+
+export class PreviewPage extends React.Component {
+  render() {
+    return (
+      <PageLayout searchState={this.props.searchState} searchActions={this.props.searchActions} profileState={this.props.profileState}>
+        <WidgetContainer
+          widgetLocationName="previewPage"
+          widgetState={this.props.widgetState}
+          widgetActions={this.props.widgetActions} />
+      </PageLayout>
+    );
+  }
+}
+
+PreviewPage.displayName = 'PreviewPage';
+PreviewPage.propTypes = {
+  profileState: React.PropTypes.object.isRequired,
+  widgetState: React.PropTypes.object.isRequired,
+  widgetActions: React.PropTypes.object.isRequired,
+  searchState: React.PropTypes.object.isRequired,
+  searchActions: React.PropTypes.object.isRequired
+};
+
+export default connect(
+  // Map redux state to group prop
+  (state) => {
+    return {
+      profileState: state.profileReducer,
+      searchState: state.searchReducer,
+      widgetState: state.widgetReducer
+    };
+  },
+
+  // Map group actions to actions props
+  (dispatch) => {
+    return {
+      searchActions: bindActionCreators(searchActions, dispatch),
+      widgetActions: bindActionCreators(widgetActions, dispatch)
+    };
+  }
+)(PreviewPage);

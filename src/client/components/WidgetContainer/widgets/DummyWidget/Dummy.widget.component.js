4c28980209406a6e84da465a86b0f49532e1cf10
Jonas Peter Hyatt <jph@dbc.dk>
2016-07-21 09:50:18 +0200

    bug-145: AbstractWidget now defines a callServiceProvider helper method.

--- src/client/components/WidgetContainer/widgets/DummyWidget/Dummy.widget.component.js
+++ src/client/components/WidgetContainer/widgets/DummyWidget/Dummy.widget.component.js
@@ -2,13 +2,16 @@
  * @file: Dummy widget used for tests, and an example of the simplest widget possible
  */
 
-import React, {Component, PropTypes} from 'react';
+import React from 'react';
+import {AbstractWidget} from '../../AbstractWidget.component';
 
 // A widget is just a normal react component, with a specific subset of props.
+// It can inherit from AbstractWidget, but it's not required.
+// AbstractWidget just provides a few sane defaults and helper methods.
 // Generally a widget should get all initial data from the CMS that controls it,
 // And request whatever remains, via our actions.
 // All data received after the CMS data, for a specific widget should be namespaced in the reducer to the widget name.
-export class DummyWidget extends Component {
+export class DummyWidget extends AbstractWidget {
   render() {
     return (
       <span className="dummy-widget">This is a dummy widget!</span>
@@ -17,22 +20,3 @@ export class DummyWidget extends Component {
 }
 
 DummyWidget.displayName = 'DummyWidget';
-
-// These are all the props a widget gets
-DummyWidget.propTypes = {
-  // This is an object containing all the actions in widget.actions.js
-  widgetActions: PropTypes.object.isRequired,
-
-  // This is an object containing data as passed from the CMS.
-  widgetConfig: PropTypes.object.isRequired,
-
-  // This is a string, with the name of the widget location (for example: FrontPageTop)
-  // This is how we specify where a specific widget goes.
-  widgetLocationName: PropTypes.string.isRequired,
-
-  // This is an object containing the property from the widgetReducer that matches the widgetName.
-  widgetReducerProp: PropTypes.object,
-
-  // This is the entire state from the widgetReducer.
-  widgetState: PropTypes.object.isRequired
-};

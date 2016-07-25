ecfd7463842217d4a790568712b86d690b25d8a3
Jonas Peter Hyatt <jph@dbc.dk>
2016-07-25 10:20:10 +0200

    sd-512: Create additional test for latestReviews widget

--- src/client/components/WidgetContainer/__mocks__/Review.mock.js
+++ src/client/components/WidgetContainer/__mocks__/Review.mock.js
@@ -27,3 +27,81 @@ export const singleReviewMock = {
   },
   image: null
 };
+
+export const singleCampaignReviewMock = {
+  pid: '870970-basis:06903177',
+  libraryid: '100451',
+  worktype: 'book',
+  content: 'asdf',
+  created: '2016-06-13T13:43:32.000Z',
+  modified: '2016-06-13T13:43:32.000Z',
+  rating: 5,
+  markedAsDeleted: null,
+  palleid: null,
+  id: 39,
+  reviewownerid: 323,
+  likes: [],
+  video: {
+    id: 98,
+    postVideoCollection: null,
+    commentVideoCollection: null,
+    reviewVideoCollection: 39,
+    resolutions: [{
+      size: 'original_video',
+      id: 2685,
+      videoCollectionResolutionId: 98,
+      imageCollectionResolutionId: null,
+      video: {
+        container: 'uxdev-biblo-input-videobucket',
+        name: '1465825408820_profile_323.mp4',
+        type: 'video/mp4',
+        url: 'api/fileContainers/uxdev-biblo-input-videobucket/download/1465825408820_profile_323.mp4',
+        id: 2696,
+        resolutionImageFileId: null,
+        resolutionVideoFileId: 2685
+      }
+    }, {
+      size: '854x480',
+      id: 2686,
+      videoCollectionResolutionId: 98,
+      imageCollectionResolutionId: null,
+      video: {
+        container: 'uxdev-biblo-output-videobucket',
+        name: '1465825408820_profile_323.mp4',
+        type: 'video/mp4',
+        url: 'api/fileContainers/uxdev-biblo-output-videobucket/download/1465825408820_profile_323.mp4',
+        id: 2697,
+        resolutionImageFileId: null,
+        resolutionVideoFileId: 2686
+      }
+    }]
+  },
+  owner: {
+    raw: {
+      description: 'afasda',
+      displayName: 'jonas'
+    },
+    description: '“afasda”',
+    displayName: 'jonas',
+    id: 323,
+    groups: [],
+    image: '/billede/326/small',
+    isModerator: false
+  },
+  campaign: {
+    campaignName: 'Sommerbogen 2016',
+    startDate: '2016-06-09T20:00:00.044Z',
+    endDate: '2016-08-21T04:00:00.044Z',
+    logos: {
+      svg: '/sommerbogen-logo.svg',
+      small: '/sommerbogen-logo.png',
+      medium: '/sommerbogen-logo.png',
+      large: '/sommerbogen-logo.png'
+    },
+    type: 'review',
+    id: 1,
+    workTypes: ['book', 'audiobook', 'literature']
+  },
+  image: null,
+  html: 'asdf'
+};
--- src/client/components/WidgetContainer/__tests__/LatestReviews.widget.test.js
+++ src/client/components/WidgetContainer/__tests__/LatestReviews.widget.test.js
@@ -7,7 +7,7 @@ import expect from 'expect';
 import {renderWidgetWithTeaspoon} from './widgetTest.utils';
 
 // import mocks
-import {singleReviewMock} from '../__mocks__/Review.mock';
+import {singleReviewMock, singleCampaignReviewMock} from '../__mocks__/Review.mock';
 
 describe('Test LatestReviews Widget', () => {
   it('Test LatestReviews widget can render inside a WidgetContainer', () => {
@@ -48,5 +48,33 @@ describe('Test LatestReviews Widget', () => {
 
     const reviewStars = $root.find('.compact-review--review-content--rating').text();
     expect(reviewStars).toEqual('★★★★★★');
+  });
+
+  it('Should render a campaign logo when a campaign is attached', () => {
+    const coverImagesState = {};
+    coverImagesState[singleCampaignReviewMock.pid] = '/images/covers/book.png';
+
+    const $root = renderWidgetWithTeaspoon({
+      location: 'test-latest-review-widget-location',
+      widgetName: 'LatestReviewsWidget',
+      widgetConfig: {
+        reviewsToLoad: 1,
+        campaignId: 1
+      },
+      state: {
+        LatestReviews: {
+          reviews: [],
+          campaignReviews: {
+            '1': [singleCampaignReviewMock]
+          },
+          campaign: singleCampaignReviewMock.campaign,
+          reviewsPending: false
+        },
+        CoverImages: coverImagesState
+      }
+    });
+
+    const campaignLogoSrc = $root.find('.latest-reviews-widget--campaign-logo > img').unwrap().src;
+    expect(campaignLogoSrc).toContain(singleCampaignReviewMock.campaign.logos.small);
   });
 });

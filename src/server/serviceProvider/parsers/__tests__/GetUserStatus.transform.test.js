6a39af3a967dd5b8bb80ba158398bcfc4496a2a2
Martin Møller <mmj@dbc.dk>
2016-09-09 12:00:58 +0200

    sd-577: Moved parsing of response from OpenUserStatus from GetUserStatus.transform.js to userstatus.parser.js to enable usage of the parsing across application

---
+++ src/server/serviceProvider/parsers/__tests__/GetUserStatus.transform.test.js
@@ -0,0 +1,27 @@
+import {assert} from 'chai';
+
+import {parseGetUserStatusResponse} from '../userstatus.parser';
+import responseMock from '../mocks/response.mock.json';
+import data from '../mocks/data.mock.json';
+
+describe('Testing the GetUserStatus Transform', () => {
+  it('Should return object as expected  - if no pickudDate is present order.ready should be set based on orderStatus', () => {
+    // Start by reading the initial data
+    const response = responseMock;
+    const expected = JSON.stringify(data);
+
+    // Mock Date.now() to ensure consistency across runs.
+    let _now = Date.now;
+    Date.now = () => 1467928800000; // 2016-07-08
+
+    // Run the transform
+    const result = parseGetUserStatusResponse(response);
+    const resultStringified = JSON.stringify(result);
+
+    // Check the result against the saved result
+    assert.equal(expected, resultStringified);
+
+    // Restore the mock.
+    Date.now = _now;
+  });
+});

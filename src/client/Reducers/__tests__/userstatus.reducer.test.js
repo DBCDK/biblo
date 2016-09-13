9bb8403d97e159f7f21edf5f685a608c420faa9d
Martin Møller <mmj@dbc.dk>
2016-09-13 12:21:57 +0200

    sd-577: Testing userstatus.reducer.js

--- src/client/Reducers/__tests__/userstatus.reducer.test.js
+++ src/client/Reducers/__tests__/userstatus.reducer.test.js
@@ -4,12 +4,22 @@
  */
 
 import {assert} from 'chai';
-import assignToEmpty from '../../Utils/assign';
-import {USERSTATUS_RENEW_LOAN} from '../../Constants/action.constants';
+import sinon from 'sinon';
+import {USERSTATUS_RENEW_LOAN, USERSTATUS_GET_STATUS} from '../../Constants/action.constants';
 
 import reducer from '../userstatus.reducer';
 
 describe('Testing the userstatus.reducer', () => {
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('Should non-manipulated state when response is empty', () => {
     const state = {
       renewLoan: {},
@@ -49,5 +59,97 @@ describe('Testing the userstatus.reducer', () => {
     assert.equal(res.renewLoan[action.response.loanId].message, action.response.message);
     assert.equal(res.renewLoan[action.response.loanId].error, action.response.error);
     assert.equal(res.renewLoan[action.response.loanId].userstatusError, action.response.userstatusError);
+  });
+
+  it('Should log an error when errors.length > 1', () => {
+    const spy = sandbox.spy(console, 'error');
+
+    const state = {
+      renewLoan: {},
+      userStatus: {}
+    };
+
+    const action = {
+      type: USERSTATUS_GET_STATUS,
+      response: {
+        errors: ['a']
+      }
+    };
+
+    assert.isFalse(spy.called);
+    reducer(state, action);
+    assert.isTrue(spy.called);
+    const args = spy.firstCall.args;
+    assert.equal(args[0], 'Error in response from OpenUserStatus');
+  });
+
+  it('Should log an error when response.result is undefined', () => {
+    const spy = sandbox.spy(console, 'error');
+
+    const state = {
+      renewLoan: {},
+      userStatus: {}
+    };
+
+    const action = {
+      type: USERSTATUS_GET_STATUS,
+      response: {
+        errors: null
+      }
+    };
+
+    assert.isFalse(spy.called);
+    reducer(state, action);
+    assert.isTrue(spy.called);
+    const args = spy.firstCall.args;
+    assert.equal(args[0], 'Error in response from OpenUserStatus');
+  });
+
+  it('Should assign response.result ito state.userStatus', () => {
+    const state = {
+      renewLoan: {},
+      userStatus: {}
+    };
+
+    const action = {
+      type: USERSTATUS_GET_STATUS,
+      response: {
+        errors: null,
+        result: {
+          someuserstatus: 'someuserstatus'
+        }
+      }
+    };
+
+    const res = reducer(state, action);
+
+    const responseString = JSON.stringify(action.response.result);
+    const resultString = JSON.stringify(res.userStatus);
+
+    assert.equal(responseString, resultString);
+  });
+
+  it('Should return state as it is when no macthing action is found', () => {
+    const state = {
+      renewLoan: {},
+      userStatus: {}
+    };
+
+    const action = {
+      type: 'NO_MATCHING_ACTION',
+      response: {
+        errors: null,
+        result: {
+          someuserstatus: 'someuserstatus'
+        }
+      }
+    };
+
+    const res = reducer(state, action);
+
+    const stateString = JSON.stringify(state);
+    const resultString = JSON.stringify(res);
+
+    assert.equal(stateString, resultString);
   });
 });

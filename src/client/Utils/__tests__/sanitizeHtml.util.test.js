51e8a119e3e222c8ac2440530448e88bed3a257a
Martin Møller <m@hrmoller.dk>
2017-11-06 20:55:20 +0100

    Renamed tests

--- src/client/Utils/__tests__/sanitizeHtml.util.test.js
+++ src/client/Utils/__tests__/sanitizeHtml.util.test.js
@@ -2,7 +2,7 @@ import {expect} from 'chai';
 import sanitizeHtml from './../sanitizeHtml.util';
 
 describe('Testing sanitizeHtml.util', () => {
-  it('Should remove unsafe HTML', () => {
+  it('Should remove content of script-tag and return safe HTML', () => {
     const html = '<div><script>alert("alert");</script></div>';
     const expected = '<div></div>';
     const result = sanitizeHtml(html);
@@ -10,7 +10,7 @@ describe('Testing sanitizeHtml.util', () => {
     expect(result).to.equal(expected);
   });
 
-  it('Should remove unsafe HTML', () => {
+  it('Should remove unsafe HTML and return empty string', () => {
     const html = '<script>alert("alert");</script>';
     const expected = '';
     const result = sanitizeHtml(html);
@@ -18,7 +18,7 @@ describe('Testing sanitizeHtml.util', () => {
     expect(result).to.equal(expected);
   });
 
-  it('Should remove unsafe HTML', () => {
+  it('Should remove script-tag and return empty string', () => {
     const html = '<script></script>';
     const expected = '';
     const result = sanitizeHtml(html);
@@ -26,7 +26,7 @@ describe('Testing sanitizeHtml.util', () => {
     expect(result).to.equal(expected);
   });
 
-  it('Should remove unsafe HTML', () => {
+  it('Should return safe img-tag', () => {
     const html = '<img src="src"></img>';
     const expected = '<img src="src">';
     const result = sanitizeHtml(html);
@@ -36,7 +36,7 @@ describe('Testing sanitizeHtml.util', () => {
     expect(result).to.equal(expected);
   });
 
-  it('Should remove unsafe HTML', () => {
+  it('Should return original string not containing HTML', () => {
     const html = 'string_string_string';
     const expected = 'string_string_string';
     const result = sanitizeHtml(html);

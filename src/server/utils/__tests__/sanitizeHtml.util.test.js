c6e58c1b2d3857604dc84d62d83c94ed82a1df99
Martin Møller <m@hrmoller.dk>
2017-12-28 22:52:33 +0100

    Added server side sanitizer

---
+++ src/server/utils/__tests__/sanitizeHtml.util.test.js
@@ -0,0 +1,44 @@
+import {expect} from 'chai';
+import sanitizeHtml from '../serverSideSanitizeHtml.util';
+
+describe('Testing sanitizeHtml.util', () => {
+  it('Should remove content of script-tag and return safe HTML', () => {
+    const html = '<div><script>alert("alert");</script></div>';
+    const expected = '<div></div>';
+    const result = sanitizeHtml(html);
+
+    expect(result).to.equal(expected);
+  });
+
+  it('Should remove unsafe HTML and return empty string', () => {
+    const html = '<script>alert("alert");</script>';
+    const expected = '';
+    const result = sanitizeHtml(html);
+
+    expect(result).to.equal(expected);
+  });
+
+  it('Should remove script-tag and return empty string', () => {
+    const html = '<script></script>';
+    const expected = '';
+    const result = sanitizeHtml(html);
+
+    expect(result).to.equal(expected);
+  });
+
+  it('Should return safe img-tag', () => {
+    const html = '<img src="src"></img>';
+    const expected = '<img src="src">';
+    const result = sanitizeHtml(html);
+
+    expect(result).to.equal(expected);
+  });
+
+  it('Should return original string not containing HTML', () => {
+    const html = 'string_string_string';
+    const expected = 'string_string_string';
+    const result = sanitizeHtml(html);
+
+    expect(result).to.equal(expected);
+  });
+});

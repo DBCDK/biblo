d742181b27006b7313ec7c3a264d00d9a5c5dcdc
Martin Møller <m@hrmoller.dk>
2018-01-19 11:02:23 +0100

    added jest: true to eslint config / removed // eslint-disable-line no-undef

--- src/client/components/CookieWarning/__tests__/CookieWarningContainer.test.js
+++ src/client/components/CookieWarning/__tests__/CookieWarningContainer.test.js
@@ -14,19 +14,19 @@ describe('Testing the CookieWarningContainer component', () => {
     Cookies.remove('reddi-fe-cookie');
     const tree = shallow(<CookieWarningContainer />);
 
-    expect(tree.find('.cookie-warning')).toHaveLength(1); // eslint-disable-line no-undef
-    expect(tree).toMatchSnapshot(); // eslint-disable-line no-undef
+    expect(tree.find('.cookie-warning')).toHaveLength(1);
+    expect(tree).toMatchSnapshot();
   });
 
   it('Should hide the cookie warning', () => {
     const tree = shallow(<CookieWarningContainer />);
     const instance = tree.instance();
 
-    expect(instance.state.displayWarning).toBe(true); // eslint-disable-line no-undef
+    expect(instance.state.displayWarning).toBe(true);
     instance.onClose();
     tree.update();
 
-    expect(instance.state.displayWarning).toBe(false); // eslint-disable-line no-undef
-    expect(tree.find('.cookie-warning')).toHaveLength(0); // eslint-disable-line no-undef
+    expect(instance.state.displayWarning).toBe(false);
+    expect(tree.find('.cookie-warning')).toHaveLength(0);
   });
 });

822ae6b9f27df2faa9fc44adef9eb492358f46b2
Yousef Mohsen <yousefmohsen@outlook.dk>
2018-06-27 14:45:33 +0200

    lint error fixed in review component

--- src/client/components/Review/Review.component.js
+++ src/client/components/Review/Review.component.js
@@ -525,7 +525,7 @@ export default class Review extends UploadMedia {
               >
                 <div className="review-add--input">
                   <textarea
-                    autofocus="true"
+                    autoFocus="true"
                     className="review-add--textarea"
                     name="content"
                     placeholder="Skriv din anmeldelse her"

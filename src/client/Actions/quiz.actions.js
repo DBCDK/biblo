cb78a554b93da4a48ed0ede033614b3569533b3c
kfudbc <kfudbc@users.noreply.github.com>
2018-10-10 08:36:19 +0200

    send library stats to quiz, some error handling (#888)

--- src/client/Actions/quiz.actions.js
+++ src/client/Actions/quiz.actions.js
@@ -1,7 +1,7 @@
 import * as types from '../Constants/action.constants';
 import request from 'superagent';
 import SocketClient from 'dbc-node-serviceprovider-socketclient';
-
+import load from 'load-script';
 const storeQuizResult = SocketClient('storeQuizResult');
 const getQuizResult = SocketClient('getQuizResult');
 
@@ -23,6 +23,28 @@ const shouldSaveResult = (prevResult, result) => {
   }
   return prevResult.score < result.score;
 };
+
+const sendQuizStats = (quizId, type, subtype, quizLibraryUrl, openPlatformToken) => {
+  return new Promise((resolve, reject) => {
+    load(quizLibraryUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      try {
+        // eslint-disable-next-line no-undef
+        openPlatformQuiz.statisticsEvent({
+          openPlatformToken,
+          quizId,
+          type,
+          subtype
+        });
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+};
 const getResultForUser = quizId => {
   return new Promise(resolve => {
     getQuizResult.request({
@@ -34,18 +56,23 @@ const getResultForUser = quizId => {
     });
   });
 };
-const saveResultForUser = async (quizId, result, libraryId) => {
-  const prevResult = await getResultForUser(quizId);
-  if (shouldSaveResult(prevResult, result)) {
-    storeQuizResult.request({
-      quizId,
-      result,
-      libraryId
-    });
+const saveResultForUser = async (quizId, result, libraryId, quizLibraryUrl, openPlatformToken) => {
+  try {
+    await sendQuizStats(quizId, 'agency', libraryId, quizLibraryUrl, openPlatformToken);
+    const prevResult = await getResultForUser(quizId);
+    if (shouldSaveResult(prevResult, result)) {
+      storeQuizResult.request({
+        quizId,
+        result,
+        libraryId
+      });
+    }
+  } catch (e) {
+    throw 'Der skete en fejl da quiz-resultatet skulle gemmes.';
   }
 };
 
-export function asyncInitializeQuiz(quizId, {userIsLoggedIn, favoriteLibrary}) {
+export function asyncInitializeQuiz(quizId, {userIsLoggedIn, favoriteLibrary, openPlatformToken}, quizLibraryUrl) {
   return async dispatch => {
     const action = {
       type: types.SET_QUIZ,
@@ -56,52 +83,88 @@ export function asyncInitializeQuiz(quizId, {userIsLoggedIn, favoriteLibrary}) {
       }
     };
 
-    let sessionResult;
     try {
-      sessionResult = (await getSession(quizId)).body;
-    } catch (err) {
-      // swallow - nothing to do if it fails
-    }
-
-    if (sessionResult) {
-      action.quiz.completed = true;
-      action.quiz.result = sessionResult;
-
-      if (userIsLoggedIn) {
-        action.quiz.stored = true;
-        await saveResultForUser(quizId, sessionResult, favoriteLibrary && favoriteLibrary.libraryId);
-        action.quiz.storedAfterLogin = true;
-        await delSession(quizId);
+      let sessionResult;
+      try {
+        sessionResult = (await getSession(quizId)).body;
+      } catch (err) {
+        // swallow - nothing to do if it fails
       }
-    } else if (userIsLoggedIn) {
-      const storedResult = await getResultForUser(quizId);
-      if (storedResult) {
-        action.quiz.result = storedResult;
+
+      if (sessionResult) {
         action.quiz.completed = true;
-        action.quiz.stored = true;
-      }
-    }
+        action.quiz.result = sessionResult;
 
-    dispatch(action);
+        if (userIsLoggedIn) {
+          action.quiz.stored = true;
+          await saveResultForUser(
+            quizId,
+            sessionResult,
+            favoriteLibrary && favoriteLibrary.libraryId,
+            quizLibraryUrl,
+            openPlatformToken
+          );
+          action.quiz.storedAfterLogin = true;
+          await delSession(quizId);
+        }
+      } else if (userIsLoggedIn) {
+        const storedResult = await getResultForUser(quizId);
+        if (storedResult) {
+          action.quiz.result = storedResult;
+          action.quiz.completed = true;
+          action.quiz.stored = true;
+        }
+      }
+      dispatch(action);
+    } catch (error) {
+      dispatch({
+        type: types.SET_QUIZ,
+        quiz: {
+          id: quizId,
+          error
+        }
+      });
+    }
   };
 }
 
-export function asyncSaveQuizResult(quizId, result, {userIsLoggedIn, favoriteLibrary}) {
+export function asyncSaveQuizResult(
+  quizId,
+  result,
+  {userIsLoggedIn, favoriteLibrary, openPlatformToken},
+  quizLibraryUrl
+) {
   return async dispatch => {
-    if (userIsLoggedIn) {
-      await saveResultForUser(quizId, result, favoriteLibrary && favoriteLibrary.libraryId);
-    } else {
-      await setSession(quizId, result);
-    }
-    dispatch({
-      type: types.SET_QUIZ,
-      quiz: {
-        id: quizId,
-        completedNow: true,
-        completed: true,
-        result
+    try {
+      if (userIsLoggedIn) {
+        await saveResultForUser(
+          quizId,
+          result,
+          favoriteLibrary && favoriteLibrary.libraryId,
+          quizLibraryUrl,
+          openPlatformToken
+        );
+      } else {
+        await setSession(quizId, result);
       }
-    });
+      dispatch({
+        type: types.SET_QUIZ,
+        quiz: {
+          id: quizId,
+          completedNow: true,
+          completed: true,
+          result
+        }
+      });
+    } catch (error) {
+      dispatch({
+        type: types.SET_QUIZ,
+        quiz: {
+          id: quizId,
+          error
+        }
+      });
+    }
   };
 }
 
